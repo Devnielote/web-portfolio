86fb2defdb5c976f33a4178c6205645ca078d50d
Devnielote <danielfc_26@hotmail.com>
2025-06-29 09:59:09 -0600

    feat: create home and project pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="hero_container text-4xl text-left font-bold">
+        <h2>Web developer,</h2>
+        <h2>frontend artisan,</h2>
+        <h2>api whisperer.</h2>
+      </div>
+      <div className="announcements">
+        <span>Announcemt componente here</span>
+      </div>
+      <div className="carrousel">
+        <div className="projects_container">Project component here</div>
+        <div className="projects_container">Project component here</div>
+        <div className="projects_container">Project component here</div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import Footer from '../components/Footer';
+
+export default function Projects () {
+  return (
+    <div className='project_container'>
+      <header>
+        <h1>Selected projects</h1>
+        <span>FROM 2020-now</span>
+      </header>
+      <div className='header_project_info'>
+        <img src="" alt="arrow pointing down"/>
+        <p>Cool projects, cool results that push me to another level (mejorar esto)</p>
+      </div>
+      <div>Project componente here</div>
+      <div>Project componente here</div>
+      <div>Project componente here</div>
+      <div>Project componente here</div>
+    </div>
+  )
+}
