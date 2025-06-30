e101f4b7127b61180bff0b671f51fce348db8952
Devnielote <danielfc_26@hotmail.com>
2025-06-30 11:01:25 -0600

    feat: add inline styles to home page components and implement project carousel component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { projects } from "../interfaces/projects";
+import ProjectCard from "../components/ProjectCard";
+import ProjectCarousel from "../components/ProjectCarousel";
 
 export default function Home() {
   return (
     <div>
-      <div className="hero_container text-4xl text-left font-bold">
+      <div className="hero_container text-[2.5rem] leading-9 text-left font-bold p-3">
         <h2>Web developer,</h2>
         <h2>frontend artisan,</h2>
         <h2>api whisperer.</h2>
       </div>
       <div className="announcements">
-        <span>Announcemt componente here</span>
+        <span>AVAILABLE FOR WORK</span>
       </div>
-      <div className="carrousel">
-        <div className="projects_container">Project component here</div>
-        <div className="projects_container">Project component here</div>
-        <div className="projects_container">Project component here</div>
+      <div className="carrousel flex w-full min-w-72 mt-3 p-10 py-14 overflow-x-scroll">
+        <ProjectCard {...projects[0]}/>
+        <ProjectCard {...projects[1]}/>
+        <ProjectCard {...projects[2]}/>
       </div>
     </div>
   )
