11b18b6011f664fdddf037be6b3c18d12777a4c8
Devnielote <danielfc_26@hotmail.com>
2025-06-30 10:57:40 -0600

    feat: add styles to footer cointainer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,33 @@ import { Link } from "react-router-dom";
 export default function Footer () {
   return (
     <footer>
-      <div className="footer_navigation">
-        <div className="reach_me">
-          <Link to="/">daniel.flores@dev.com</Link>
+      <div className="footer_navigation px-3 pt-10 mt-10">
+        <div className="reach_me w-full flex-col text-left"> 
+          <span>/CONTACT ME</span>
+          <div className="text-lg mt-2">
+            <Link to="/">daniel.flores@dev.com</Link>
+          </div>
         </div>
-        <div className="footer_links">
-          <Link to="/home">Home</Link>
-          <Link to="/projects">Selected projects</Link>
-          <Link to="/home">About</Link>
-        </div>
-        <div className="socials">
-          <Link to="https://www.linkedin.com/in/danielflorescardona/">Linkedin</Link>
-          <Link to="https://github.com/Devnielote">Github</Link>
-          <Link to="/home">Resume</Link>
+        <div className="socials flex-col text-left mt-4">
+          <span>/STALK ME</span>
+          <div className="flex-col mt-2">
+            <Link className="flex w-auto" to="https://www.linkedin.com/in/danielflorescardona/">Linkedin</Link>
+            <Link className="flex" to="https://github.com/Devnielote">Github</Link>
+            <Link className="flex" to="/home">Resume</Link>
+          </div>
+          <div className="footer_links flex-col mt-4">
+            <span>/NAVIGATE TO</span>
+            <Link to="/home" className="flex mt-2">Home</Link>
+            <Link to="/projects" className="flex">Selected projects</Link>
+            <Link to="/home" className="flex">About</Link>
+          </div>
         </div>
         <div>
-          <Link to="/home">
-            <h1>Let's create together --- <img src="" alt=""/></h1>
+          <Link to="/home" className="text-left">
+            <h1 className="text-6xl">Let's create together<img src="" alt=""/></h1>
           </Link>
         </div>
-        <p>DESIGNED WITH 💚 BY ME</p>
+        <p className="mt-5 py-5">DESIGNED WITH 💚 BY ME</p>
       </div>
     </footer>
   )
