89466caabc3ccfb38663fe34645d2fdf584ca297
Devnielote <danielfc_26@hotmail.com>
2025-07-01 21:27:13 -0600

    chore: delete unused modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -8,7 +5,6 @@ import Projects from './pages/Projects'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
