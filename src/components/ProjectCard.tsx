b6d00b45f667de82f57743840eef27ea635c8410
Devnielote <danielfc_26@hotmail.com>
2025-07-01 21:30:52 -0600

    styles: resposive width to the project card images

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import type { Project } from '../interfaces/projects';
 
 export default function ProjectCard(props: Project) {
   const {index,title,description,liveUrl,imageUrl,tags} = props;
 
   return(
-    <div className='project_container flex-col align-middle  mb-2 mr-44'>
-      <header className='mb-3 text-2xl font-bold'>
-        <h1>{title}</h1>
-      </header>
-      <div className="image_container w-72 m-auto mb-7">
-        <img src={imageUrl} alt="" className='w-full'/>
+    <div className='project_container flex-col align-middle  mb-2'>
+      <div className="image_container w-10/12 m-auto mb-7 sm:w-5/12 lg:w-4/5">
+        <Link to="/home">
+          <img src={imageUrl} alt="" className='w-full'/>
+        </Link>
       </div>
-      <div className="tag_container text-[var(--green-phosphor)]">
+      <div className="tag_container mb-2 text-[var(--green-phosphor)]">
         {tags.map((tag,index) => (
           <span key={index} className='project_tag mr-2'>
             {tag}
