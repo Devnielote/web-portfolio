1a5873e857f64bb0929b98088059d0c792f3683f
Devnielote <danielfc_26@hotmail.com>
2025-07-01 21:32:39 -0600

    feat: add infinite loop to slider and change pagination bullets position and styles

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -1,27 +1,35 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules'
 import 'swiper/css'; // estilo base
+import 'swiper/css/pagination';
 
 import ProjectCard from './ProjectCard';
-import { projects } from '../interfaces/projects';
+import { mainProjects } from '../interfaces/projects';
 import type { Project } from '../interfaces/projects';
 
 export default function ProjectCarousel() {
   return (
-    <Swiper
-      spaceBetween={16}
-      slidesPerView={1}
-      breakpoints={{
-        640: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 },
-      }}
-    >
-      {projects.map((project: Project) => (
-        <SwiperSlide key={project.title}>
-          <ProjectCard {...project} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        className='relative mt-3'
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          1024: { slidesPerView: 3 },
+        }}
+        pagination={{clickable: true}}
+        scrollbar={{draggable: false}}
+        loop={true}
+      >
+        {mainProjects.map((project: Project) => (
+          <SwiperSlide className='hover:brightness-125 hover:drop-shadow-[0_0_5px_#42ae38]' key={project.title}>
+            <ProjectCard {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
 
