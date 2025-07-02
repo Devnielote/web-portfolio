7adb36ccc376ede3e7f3d0d2d0a83ff5ddfa644d
Devnielote <danielfc_26@hotmail.com>
2025-07-02 11:07:21 -0600

    styles: minimal styles changes to the annoucements container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,13 @@ import ProjectCarousel from "../components/ProjectCarousel";
 
 export default function Home() {
   return (
-    <div>
-      <div className="hero_container text-[2.5rem] leading-9 text-left font-bold p-3">
+    <div className="mt-10">
+      <div className="hero_container text-[2.5rem] leading-9 text-left font-bold p-3 md:text-[3rem] md:leading-10">
         <h2>Web developer,</h2>
         <h2>frontend artisan,</h2>
         <h2>api whisperer.</h2>
       </div>
-      <div className="announcements mt-3 pl-2 text-left">
+      <div className="announcements mt-3 pl-2 text-left md:pl-4">
         <span>AVAILABLE FOR WORK</span>
       </div>
       <ProjectCarousel/>
