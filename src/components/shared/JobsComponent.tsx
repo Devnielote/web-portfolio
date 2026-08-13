282da532da08311969c9fa0817001888c40be181
Devnielote <danielfc_26@hotmai.com>
2026-08-13 06:30:23 -0600

    feature(components): add job interface

--- src/components/shared/JobsComponent.tsx
+++ src/components/shared/JobsComponent.tsx
@@ -1,6 +1,39 @@
+interface Job {
+  title: string;
+  company: string;
+  startDate: Date | string;
+  endDate: Date | string;
+}
+
+const jobList: Job[] = [
+  {
+    title: "Full-stack developer",
+    company: "Citro Telecom",
+    startDate: new Date("2025-09-01"),
+    endDate: "Present",
+  },
+  {
+    title: "Front-end developer",
+    company: "Freelance",
+    startDate: new Date("2022-01-01"),
+    endDate: "Present",
+  },
+  {
+    title: "Front-end developer",
+    company: "Outline Creative Community",
+    startDate: new Date("2021-01-01"),
+    endDate: "Present",
+  },
+
+]
+
+
 export default function JobsComponent() {
   return (
     <div className="mt-10 lg:w-[calc(50vw)]">
+      {
+
+      }
       <div className="flex justify-between items-center">
         <h1>
           Frontend developer -{" "}
