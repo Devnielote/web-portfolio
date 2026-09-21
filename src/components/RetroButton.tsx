a141315f442eec938ae1d837f39c2c788341342e
Devnielote <danielfc_26@hotmail.com>
2026-09-20 22:02:09 -0600

    chore(package update): quick packages upgrade and linkedin url fix

--- src/components/RetroButton.tsx
+++ src/components/RetroButton.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function RetroButton() {
   return (
-    <Link className="" to="https://www.linkedin.com/in/danielflorescardona/">
+    <Link className="" to="https://www.linkedin.com/in/danielfloca">
       <button className="button text-sm md:text-lg">VIEW LINKEDIN ↱</button>
     </Link>
   );
